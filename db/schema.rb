10570de0b15d936ba5ac5bcd90b28c38fe868f38
miura <s.miura@mof-mof.co.jp>
2022-08-19 10:15:51 +0900

    schemaファイル修正

--- db/schema.rb
+++ db/schema.rb
@@ -21,14 +21,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_18_090946) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
-    t.string "crypted_password"
-    t.string "salt"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
-
 end
