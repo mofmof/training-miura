ad135660c9927452079898774812f72a5ee51298
miura <s.miura@mof-mof.co.jp>
2022-08-22 15:04:06 +0900

    manifest.js修正-rev1

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,7 +2,3 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
-if (process.env.NODE_ENV === 'development') {
-  //= link graphiql/rails/application.css
-  //= link graphiql/rails/application.js
-}
