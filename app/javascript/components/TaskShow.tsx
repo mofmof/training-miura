908fdd7c51564b763db62a1cb736cf162ca11b3b
miura <s.miura@mof-mof.co.jp>
2022-08-24 11:41:05 +0900

    型追加

--- app/javascript/components/TaskShow.tsx
+++ app/javascript/components/TaskShow.tsx
@@ -20,20 +20,20 @@ const FETCH_TASK = gql`
 type Task = {
   id: number;
   title: string;
-	body: Text;
+	body: string;
 }
 
 const TaskShow = () => {
   const { id } = useParams();
 	const { loading, error, data: {task} = {}} = useQuery(FETCH_TASK, { variables: { id: id }});
+	const taskShow: Task = task
   if (loading) return <p>Loading...</p>
   if (error) return <p>`Error! ${error.message}`;</p>
-
   return (
     <div>
-      <p>{task.id}</p>
-			<p>{task.title}</p>
-			<p>{task.body}</p>
+      <p>{taskShow.id}</p>
+			<p>{taskShow.title}</p>
+			<p>{taskShow.body}</p>
     </div>
   );
 }
