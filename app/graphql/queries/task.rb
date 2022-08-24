1dfa27b76b1527790b463022ad30a98369b48a86
miura <s.miura@mof-mof.co.jp>
2022-08-24 12:20:56 +0900

    lint修正

--- app/graphql/queries/task.rb
+++ app/graphql/queries/task.rb
@@ -2,7 +2,7 @@ module Queries
   class Task < Queries::BaseQuery
     argument :id, ID, required: true
 
-		type ObjectTypes::Task, null: false
+    type ObjectTypes::Task, null: false
 
     def resolve(id:)
       ::Task.find(id)
