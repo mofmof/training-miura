f2d0c0c9236effbfab5277729ec9aa9614fc7a0f
miura <s.miura@mof-mof.co.jp>
2022-08-30 16:10:29 +0900

    ルーティング作成

--- app/javascript/App.tsx
+++ app/javascript/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TaskShow from "./components/TaskShow";
 import TopPage from "./components/TopPage";
+import TaskUpdate from "./components/TaskUpdate";
 
 const App = () => {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<TopPage />}></Route>
-          <Route path="tasks/:id" element={<TaskShow />}></Route>
+          <Route path="/" element={<TopPage />} />
+          <Route path="tasks/:id" element={<TaskShow />} />
+          <Route path="tasks/:id/edit" element={<TaskUpdate />} />
         </Routes>
       </Router>
     </>
