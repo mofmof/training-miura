024bbe8a1b76826aabd1b7f6fc9341f1da145f6d
miura <s.miura@mof-mof.co.jp>
2022-09-01 11:17:54 +0900

    [バックエンド]タスク削除mutation作成

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_task, mutation: Mutations::CreateTask
     field :update_task, mutation: Mutations::UpdateTask
+    field :delete_task, mutation: Mutations::DeleteTask
   end
 end
