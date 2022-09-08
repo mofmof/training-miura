df040a8bb94bf87a55063195ebc563fd6a34f3cd
miura <s.miura@mof-mof.co.jp>
2022-09-08 12:26:24 +0900

    タスクの並びのデフォルトを作成日順に変更

--- app/javascript/components/Tasks.tsx
+++ app/javascript/components/Tasks.tsx
@@ -3,16 +3,23 @@ import { useTasksQuery } from "../graphql/generated";
 
 const Tasks = () => {
   const { data: { tasks = [] } = {} } = useTasksQuery();
+  const tasksSort = [...tasks];
+  const renderTasks = (taskArray: typeof tasks) => {
+    taskArray.sort((a: any, b: any) => {
+      const aTaskDate = new Date(a.createdAt);
+      const bTaskDate = new Date(b.createdAt);
+      return aTaskDate < bTaskDate ? 1 : -1;
+    });
+    return taskArray.map((task) => (
+      <li key={task.id}>
+        <Link to={`/tasks/${task.id}`}>{task.title}</Link>
+      </li>
+    ));
+  };
 
   return (
     <div>
-      <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <Link to={`/tasks/${task.id}`}>{task.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <ul>{renderTasks(tasksSort)}</ul>
     </div>
   );
 };
