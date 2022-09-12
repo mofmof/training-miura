2a73efbdd378f2dbc9238a3f0e1a2c1b3ca2907e
miura <s.miura@mof-mof.co.jp>
2022-09-12 18:47:15 +0900

    タスクの期限設定用カラム追加

---
+++ db/migrate/20220912063855_add_limit_at_to_tasks.rb
@@ -0,0 +1,5 @@
+class AddLimitAtToTasks < ActiveRecord::Migration[7.0]
+  def change
+    add_column :tasks, :limit_at, :date
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_912_044_822) do
+ActiveRecord::Schema[7.0].define(version: 20_220_912_063_855) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 20_220_912_044_822) do
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.bigint 'user_id'
+    t.date 'limit_at'
     t.index ['user_id'], name: 'index_tasks_on_user_id'
   end
 
