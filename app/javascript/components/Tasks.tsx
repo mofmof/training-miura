8990a0deed40676910e46f3b9a90874494d1278d
miura <s.miura@mof-mof.co.jp>
2022-09-13 13:58:14 +0900

    タスクの期限が終了間近や過ぎている時にメッセージを表示させる

--- app/javascript/components/Tasks.tsx
+++ app/javascript/components/Tasks.tsx
@@ -4,6 +4,35 @@ import { useTasksQuery } from "../graphql/generated";
 const Tasks = () => {
   const { data: { tasks = [] } = {} } = useTasksQuery();
 
+  const formatDate = (date: Date): string => {
+    const y: number = date.getFullYear();
+    const m: string = ("00" + (date.getMonth() + 1)).slice(-2);
+    const d: string = ("00" + date.getDate()).slice(-2);
+    return `${y + "-" + m + "-" + d}`;
+  };
+
+  const diffLimitAt = (limitAt: any) => {
+    if (!limitAt) return;
+    const LimitAtParse: Date = new Date(limitAt);
+    const today: Date = new Date(formatDate(new Date()));
+    const diffDay: number = Math.floor(
+      (LimitAtParse.getTime() - today.getTime()) / 86400000
+    );
+    const limitMessage = () => {
+      if (diffDay > 3 || diffDay == undefined) {
+        return;
+      } else if (diffDay < 1 && diffDay > -1) {
+        return "-期限当日です";
+      } else if (diffDay <= 3 && diffDay > 0) {
+        return `-期限${diffDay}日前です`;
+      } else {
+        return "-期限が過ぎています";
+      }
+    };
+
+    return limitMessage();
+  };
+
   return (
     <div>
       <ul>
@@ -11,6 +40,7 @@ const Tasks = () => {
           <li key={task.id}>
             <Link to={`/tasks/${task.id}`}>
               {task.title}-{task.limitAt}
+              {diffLimitAt(task.limitAt)}
             </Link>
           </li>
         ))}
