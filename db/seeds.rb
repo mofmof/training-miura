df5da64e2b64b7fb2f3356a14d67cc276ef75f35
miura <s.miura@mof-mof.co.jp>
2022-10-07 11:31:24 +0900

    seeds.rbのデータ生成メソッドをseedからcreateメソッドに修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 100.times do |i|
-  User.seed(name: "ユーザー#{i}", email: "user#{i}@example.com", password: "user#{i}", password_confirmation: "user#{i}")
+  User.create(name: "ユーザー#{i}", email: "user#{i}@example.com", password: "user#{i}", password_confirmation: "user#{i}")
 end
 
 100.times do |i|
-  Task.seed(title: "タイトル#{i}", body: "ボディ#{i}", user_id: 1)
+  Task.create(title: "タイトル#{i}", body: "ボディ#{i}", user_id: 1)
 end
