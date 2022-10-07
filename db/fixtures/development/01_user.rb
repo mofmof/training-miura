d27cb56657f9d93bfd638ab18d7d6849e8355215
miura <s.miura@mof-mof.co.jp>
2022-10-07 10:39:03 +0900

    テストデータ作成をseed_fuではなくseedsに戻す,gem seed_fuは削除

--- db/fixtures/development/01_user.rb
+++
@@ -1,3 +0,0 @@
-100.times do |i|
-  User.seed(name: "ユーザー#{i}", email: "user#{i}@example.com", password: "user#{i}", password_confirmation: "user#{i}")
-end
--- db/fixtures/development/02_task.rb
+++
@@ -1,3 +0,0 @@
-100.times do |i|
-  Task.seed(title: "タイトル#{i}", body: "ボディ#{i}", user_id: 1)
-end
