c9fc93d95d071d558ddcfc07a7e769988e5f4160
miura <s.miura@mof-mof.co.jp>
2022-10-12 12:25:54 +0900

    本番環境でのsentry動作確認用にバリデーションコメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  # validates :email, presence: true, uniqueness: true
   validates :role, presence: true
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
