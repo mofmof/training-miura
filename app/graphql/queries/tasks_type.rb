6ae905741cc6b485a0653ac997d2f609599363d6
miura <s.miura@mof-mof.co.jp>
2022-10-12 11:58:34 +0900

    [バックエンド]ステータスで検索できるように変更

--- app/graphql/queries/tasks_type.rb
+++ app/graphql/queries/tasks_type.rb
@@ -1,10 +1,16 @@
 module Queries
-  class TasksType < Queries::LoginRequiredQuery
+  # class TasksType < Queries::LoginRequiredQuery
+  class TasksType < Queries::BaseQuery
     argument :title, String, required: true
+    argument :state, String, required: false
     type ObjectTypes::Task.connection_type, null: false
 
-    def resolve(title:)
-      context[:current_user].tasks.find_title(title).order(created_at: :desc)
+    def resolve(args)
+      if args[:state].blank?
+        context[:current_user].tasks.find_title(args[:title]).order(created_at: :desc)
+      else
+        context[:current_user].tasks.find_title(args[:title]).where(state: args[:state]).order(created_at: :desc)
+      end
     end
   end
 end
