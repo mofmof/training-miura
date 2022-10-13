6906aa5f25a723d699b2258cd42ea9bbf012065d
miura <s.miura@mof-mof.co.jp>
2022-10-13 11:13:10 +0900

    tasks_type.rbのリファクタリング

--- app/graphql/queries/tasks_type.rb
+++ app/graphql/queries/tasks_type.rb
@@ -1,16 +1,13 @@
 module Queries
-  # class TasksType < Queries::LoginRequiredQuery
-  class TasksType < Queries::BaseQuery
+  class TasksType < Queries::LoginRequiredQuery
     argument :title, String, required: true
     argument :state, String, required: false
     type ObjectTypes::Task.connection_type, null: false
 
     def resolve(args)
-      if args[:state].blank?
-        context[:current_user].tasks.find_title(args[:title]).order(created_at: :desc)
-      else
-        context[:current_user].tasks.find_title(args[:title]).where(state: args[:state]).order(created_at: :desc)
-      end
+      tasks = context[:current_user].tasks.find_title(args[:title]).order(created_at: :desc)
+      tasks = tasks.where(state: args[:state]) if args[:state].present?
+      tasks
     end
   end
 end
