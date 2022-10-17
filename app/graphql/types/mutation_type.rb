817da67c35cad38110d9104a27dd94d35321bfea
miura <s.miura@mof-mof.co.jp>
2022-10-17 11:57:29 +0900

    [バックエンド]Label作成mutation作成

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,6 +3,7 @@ module Types
     field :create_task, mutation: Mutations::CreateTask
     field :update_task, mutation: Mutations::UpdateTask
     field :delete_task, mutation: Mutations::DeleteTask
+    field :create_label, mutation: Mutations::CreateLabel
 
     field :sign_up, mutation: Mutations::SignUp
   end
