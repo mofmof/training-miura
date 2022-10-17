506bc24bfcb82ac78d25326d1ed3811da3e844db
miura <s.miura@mof-mof.co.jp>
2022-10-17 12:08:35 +0900

    labelとtaskの中間テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_16_073702) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_16_073815) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_16_073702) do
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_labels_on_name", unique: true
     t.index ["user_id"], name: "index_labels_on_user_id"
+  end
+
+  create_table "task_labels", force: :cascade do |t|
+    t.bigint "task_id", null: false
+    t.bigint "label_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["label_id"], name: "index_task_labels_on_label_id"
+    t.index ["task_id"], name: "index_task_labels_on_task_id"
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -46,5 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_16_073702) do
   end
 
   add_foreign_key "labels", "users"
+  add_foreign_key "task_labels", "labels"
+  add_foreign_key "task_labels", "tasks"
   add_foreign_key "tasks", "users"
 end
