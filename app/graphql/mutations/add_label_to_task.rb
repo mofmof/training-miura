a88419e442f1b2d12b6b4a6f20803a8b661542fd
miura <s.miura@mof-mof.co.jp>
2022-10-18 23:23:27 +0900

    不要なコード削除

--- app/graphql/mutations/add_label_to_task.rb
+++ app/graphql/mutations/add_label_to_task.rb
@@ -8,8 +8,8 @@ module Mutations
     def resolve(task_id:, label_id:)
       task = context[:current_user].tasks.find(task_id)
       label = context[:current_user].labels.find(label_id)
-      task_label = TaskLabel.new(task_id:, label_id:)
-      if task.user == label.user && task_label.save
+      task_label = TaskLabel.new(task_id: task.id, label_id: label.id)
+      if task_label.save
         { task_label: }
       else
         raise GraphQL::ExecutionError, task_label.errors.full_messages.join("\n")
