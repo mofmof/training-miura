37c1ab2c20af43422fe042c8e9f2df139ba0fdae
miura <s.miura@mof-mof.co.jp>
2022-10-20 12:17:27 +0900

    share_task_type.rb内の変数名変更

--- app/graphql/queries/share_tasks_type.rb
+++ app/graphql/queries/share_tasks_type.rb
@@ -5,8 +5,8 @@ module Queries
     type ObjectTypes::Task.connection_type, null: false
 
     def resolve(args)
-      task_all = Task.joins(:share_tasks).includes(:share_tasks).where(share_tasks: { user_id: context[:current_user].id })
-      tasks = task_all.find_title(args[:title]).order(created_at: :desc)
+      tasks = Task.joins(:share_tasks).includes(:share_tasks).where(share_tasks: { user_id: context[:current_user].id })
+      tasks = tasks.find_title(args[:title]).order(created_at: :desc)
       tasks = tasks.where(state: args[:state]) if args[:state].present?
       tasks
     end
