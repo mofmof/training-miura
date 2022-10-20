15acd3e7b7f3d43fbf8cd444f2997d6668e4b894
miura <s.miura@mof-mof.co.jp>
2022-10-20 18:15:40 +0900

    不要なコード削除

--- app/graphql/queries/share_tasks_type.rb
+++ app/graphql/queries/share_tasks_type.rb
@@ -5,7 +5,7 @@ module Queries
     type ObjectTypes::Task.connection_type, null: false
 
     def resolve(args)
-      tasks = Task.joins(:share_tasks).includes(:share_tasks).where(share_tasks: { user_id: context[:current_user].id })
+      tasks = Task.joins(:share_tasks).where(share_tasks: { user_id: context[:current_user].id })
       tasks = tasks.find_title(args[:title]).order(created_at: :desc)
       tasks = tasks.where(state: args[:state]) if args[:state].present?
       tasks
