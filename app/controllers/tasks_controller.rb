5609293248ac58a71cf037dcef6e7cca6e284446
miura <s.miura@mof-mof.co.jp>
2022-11-09 00:06:50 +0900

    csvインポート処理をactivejobに移行

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,4 @@
 class TasksController < ApplicationController
-  require 'csv'
   before_action :require_login
   def index; end
 
@@ -10,22 +9,7 @@ class TasksController < ApplicationController
 
   def csv_import
     current_user.update(csv_file: params[:file])
-    tmp_file_path = Rails.root.join('tmp/csv_file.csv')
-    file_content = current_user.csv_file.download
-    File.binwrite(tmp_file_path, file_content)
-
-    ActiveRecord::Base.transaction do
-      CSV.foreach(tmp_file_path, headers: true) do |line|
-        current_user.tasks.create(
-          title: line[0],
-          body: line[1],
-          limit_on: line[2],
-          state: line[3]
-        )
-      end
-    end
-
-    current_user.csv_file.purge
+    ImportTasksCsvJob.perform_later(current_user.id) if current_user.csv_file.attached?
     redirect_to root_url
   end
 end
---
+++ app/jobs/import_tasks_csv_job.rb
@@ -0,0 +1,23 @@
+class ImportTasksCsvJob < ApplicationJob
+  require 'csv'
+  def perform(user_id)
+    user = User.find(user_id)
+    return false if user.blank?
+
+    tmp_file_path = Rails.root.join('tmp/csv_file.csv')
+    file_content = user.csv_file.download
+    File.binwrite(tmp_file_path, file_content)
+
+    ActiveRecord::Base.transaction do
+      CSV.foreach(tmp_file_path, headers: true) do |line|
+        user.tasks.create(
+          title: line[0],
+          body: line[1],
+          limit_on: line[2],
+          state: line[3]
+        )
+      end
+      user.csv_file.purge
+    end
+  end
+end
