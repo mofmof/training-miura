481860cf8c29ee38a88f41466da4f887c6f2f41e
miura <s.miura@mof-mof.co.jp>
2022-11-09 18:46:54 +0900

    task一覧を作成日順となるよう修正

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -2,7 +2,7 @@ module Api
   class TasksController < ApplicationController
     before_action :require_login
     def index
-      @tasks = current_user.tasks
+      @tasks = current_user.tasks.order(created_at: :desc)
       render json: @tasks, each_serializer: TaskSerializer
     end
   end
